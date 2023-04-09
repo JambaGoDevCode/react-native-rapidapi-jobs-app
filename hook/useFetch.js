014bff0e091ab63e8809ac398413706be5144efe
Joao Jamba <jambapedrojoao@gmail.com>
2023-04-09 20:23:19 +0100

    feact: first commit job app rapidapi

---
+++ hook/useFetch.js
@@ -0,0 +1,52 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+const useFetch = (endpoint, query) => {
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const options = {
+    method: "GET",
+    url: `https://jsearch.p.rapidapi.com/${endpoint}`,
+    headers: {
+      //"X-RapidAPI-Key": process.env.RAPID_API_KEY_JJ,
+     'X-RapidAPI-Key': '80418172d8mshd0eb130f79e09f2p163dd6jsnb8d1f76f02aa',
+    'X-RapidAPI-Host': 'jsearch.p.rapidapi.com'
+    },
+    params: { ...query },
+  };
+
+  const fetchData = async () => {
+    setIsLoading(true);
+
+    try {
+      const response = await axios.request(options);
+      setData(response.data.data);
+      setIsLoading(false);
+    } catch(error) {
+      setError(error);
+      alert("Samething went wrong! There is the Error");
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const refetch = () => {
+    setIsLoading(true);
+    fetchData();
+  };
+
+  return {
+    data,
+    isLoading,
+    error,
+    refetch,
+  };
+};
+
+export default useFetch;
